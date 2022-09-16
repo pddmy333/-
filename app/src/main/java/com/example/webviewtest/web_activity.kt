0bd6bb0d8818c890e596a1d94fca9c0b09248304
pddmy333 <1677910057@qq.com>
2022-09-16 15:50:21 +0800

    添加重定向

--- app/src/main/java/com/example/webviewtest/web_activity.kt
+++ app/src/main/java/com/example/webviewtest/web_activity.kt
@@ -1,22 +1,57 @@
 package com.example.webviewtest
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.KeyEvent
-import android.view.View
-import android.webkit.WebViewClient
+import android.webkit.*
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.web_layout.*
+
 
 class web_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.web_layout)
         supportActionBar?.hide()
-        webView.settings.javaScriptEnabled=true
+        val settings = webView.settings
+        settings.javaScriptEnabled=true
+        settings.javaScriptCanOpenWindowsAutomatically = true//js和android交互
+        settings.allowFileAccess = true // 允许访问文件
+        //settings.setAppCacheEnabled(true) //设置H5的缓存打开,默认关闭
+        settings.useWideViewPort = true//设置webview自适应屏幕大小
+        settings.layoutAlgorithm = WebSettings.LayoutAlgorithm.NORMAL//设置，可能的话使所有列的宽度不超过屏幕宽度
+        settings.loadWithOverviewMode = true//设置webview自适应屏幕大小
+        settings.domStorageEnabled = true//设置可以使用localStorage
+        settings.setSupportZoom(true)
+        settings.builtInZoomControls = true
+        settings.domStorageEnabled = true
         val data1=intent.getStringExtra("data1")
         val data2=intent.getStringExtra("data2")
-        webView.webViewClient= WebViewClient()
+        //webView.webViewClient=WebViewClient()
+        val mwebViewClient=object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                val redirectUrl:String= request?.url.toString()
+                if (redirectUrl.startsWith("http:")||redirectUrl.startsWith("https:")){
+                    if (view != null) {
+                        view.loadUrl(redirectUrl)
 
+                    }
+                    return true
+                }
+                //下面代码来自“https://www.jianshu.com/p/46c7e7a5bd20”用于拦截自定义协议。
+                if(!redirectUrl.startsWith("http:")||!redirectUrl.startsWith("https:")){
+                    return true
+                }
+                return super.shouldOverrideUrlLoading(view, request)
+            }
+
+        }
+        webView.webViewClient=mwebViewClient
         when(data2){
             "baidu"->{
                 if (data1 != null) {
@@ -24,12 +59,22 @@ class web_activity : AppCompatActivity() {
                         webView.loadUrl("https://www.baidu.com/")
                     else{
                         webView.loadUrl("https://www.baidu.com/s?wd=${data1}")
+
                     }
                 }
 
             }
             "bing"->{
-                webView.loadUrl("https://cn.bing.com/search?q=${data1}")
+                if (data1!=null)
+                {
+                    if (data1.length==0)
+                        webView.loadUrl("https://cn.bing.com/")
+                    else {
+                        webView.loadUrl("https://cn.bing.com/search?q=${data1}")
+
+                    }
+                }
+
             }
         }
 
@@ -38,8 +83,9 @@ class web_activity : AppCompatActivity() {
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
             if (webView!!.canGoBack()) {
-                webView.goBack()
-                return true
+                    webView.goBack()
+                    return true
+
             } else {
                 finish()
                 return false
